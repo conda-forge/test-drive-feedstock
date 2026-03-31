da2a22b30a004371c78a559d95864e6324ab6780
Sebastian Ehlert <sehlert@microsoft.com>
2026-03-31 18:14:43 +0200

    Try to use flang21 on Windows

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -1,7 +1,8 @@
 setlocal EnableExtensions EnableDelayedExpansion
 @echo on
 
-meson setup _build --prefix=%LIBRARY_PREFIX%
+if "%module_abi%"=="gfortran" (
+meson setup _build %MESON_ARGS% -Dprefix=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 meson compile -C _build
@@ -12,3 +13,20 @@ if errorlevel 1 exit 1
 
 meson install -C _build --no-rebuild
 if errorlevel 1 exit 1
+) else (
+if "%module_abi%"=="ifx" (
+set "FFLAGS=!FFLAGS! -I%BUILD_PREFIX%\opt\compiler\include -I%BUILD_PREFIX%\opt\compiler\include\intel64"
+)
+cmake -G Ninja -B _build %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS=ON -DTESTDRIVE_WITH_XDP=OFF -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON
+if errorlevel 1 exit 1
+
+cmake --build _build
+if errorlevel 1 exit 1
+
+set PATH=%CD%\_build;%PATH%
+ctest --test-dir _build --output-on-failure
+if errorlevel 1 exit 1
+
+cmake --install _build
+if errorlevel 1 exit 1
+)
